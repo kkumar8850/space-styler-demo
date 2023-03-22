01bbcfc070076392dcbbb27d81f37046a29a1bd9
Kuldeep <kuldeep.kumar@ikonwork.com>
2023-03-22 23:15:01 +0530

    add new gallery page

---
+++ pages/gallery/[id].js
@@ -0,0 +1,99 @@
+import Header from '@/components/Header/Header'
+import React from 'react'
+import Image1 from '@/public/Images/1.jpg'
+import Image2 from '@/public/Images/bedImage.jpg'
+import Image3 from '@/public/Images/bedroom_slider.jpg'
+import Image4 from '@/public/Images/drawing_room.jpg'
+import Image5 from '@/public/Images/living_room_slider.jpg'
+import Image from 'next/image'
+
+const arr = [
+    {
+        id : 0,
+        name : 'image1',
+        img : Image1
+    },
+    {
+        id : 1,
+        name : 'image1',
+        img : Image2
+    },
+    {
+        id : 2,
+        name : 'image1',
+        img : Image3
+    },
+    {
+        id : 3,
+        name : 'image1',
+        img : Image4
+    },
+    {
+        id : 4,
+        name : 'image1',
+        img : Image5
+    },
+    {
+        id : 5,
+        name : 'image1',
+        img : Image1
+    },
+]
+
+function id() {
+  return (
+    <div>
+        <Header />
+        <div 
+            className='h-[300px])'
+            style={{
+                backgroundImage : `url(/Images/1.jpg)`,
+                backgroundRepeat : 'no-repeat',
+                backgroundSize : 'cover',
+                backgroundPosition : 'center'
+            }}
+            >
+            <div 
+                className='container h-[300px] relative bg-[rgba(0,0,0,0.6)]'>
+                <div className='absolute bottom-[-30px]'>
+                    <div className='h-[200px] w-[100%] flex'>
+                        <Image
+                            src={arr[0].img}
+                            alt="test"
+                            height={200}
+                            width={200}
+                            className="rounded-full"
+                            />
+                        <div>
+                            <h1 className='text-3xl ml-[30px] font-medium'>Decor in west delhi</h1>
+                            <p className='text-xl ml-[30px] mt-4'> 
+Every interiors project and every client is unique so our approach can be flexible to suit your needs.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='container mt-[60px]'>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 mt-10 mb-10'>
+                {
+                    arr.map(a=> (
+                        <div
+                            className='h-[300px] relative w-[100%]'
+                            key={a.id}
+                        >
+                            <Image
+                                key={a.id}
+                                src={a.img}
+                                alt={a.name}
+                                fill
+                            />
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default id
---
+++ pages/gallery/index.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import Header from '@/components/Header/Header'
+import Image1 from '@/public/Images/1.jpg'
+import Image2 from '@/public/Images/2.jpg'
+import Image3 from '@/public/Images/3.jpg'
+import Image4 from '@/public/Images/4.jpg'
+import Image5 from '@/public/Images/5.jpg'
+import Image6 from '@/public/Images/6.jpg'
+import Image from 'next/image'
+
+const portfolio = [
+  {
+    id : 0,
+    name : 'Section 1',
+    img : Image1
+  },
+  {
+    id : 1,
+    name : 'Section 2',
+    img : Image2
+  },
+  {
+    id : 2,
+    name : 'Section 3',
+    img : Image3
+  },
+  {
+    id : 3,
+    name : 'Section 4',
+    img : Image4
+  },
+  {
+    id : 4,
+    name : 'Section 5',
+    img : Image5
+  },
+  {
+    id : 5,
+    name : 'Section 6',
+    img : Image6
+  },
+]
+
+function gallery() {
+  return (
+    <div>
+        <Header />
+        <div className='container'>
+          <h1 className='text-center uppercase mt-5 text-3xl'>Awesome Portfolio</h1>
+          <div className='grid lg:grid-cols-3 xs:grid-cols-1 gap-6 mt-4'>
+          {
+            portfolio.map(i=> (
+              <a href={`/gallery/${i.name}`} className="hover:mt-[-20px] transition-[0.5s]">
+                <div 
+                    className='bg-[#eaeaea] text-center p-[20px] rounded-sm ' key={i.id}>
+                    <div>
+                        <Image
+                            src={i.img}
+                            alt="second"
+                            className='w-[100%] h-[300px] mb-4'
+                        />
+                    </div>
+                    <p className='text-[#333] text-2xl font-bold mb-4'>{i.name}</p>
+                    {/* <p className='text-[#444] text-sm'>{i.subHeading}</p> */}
+                </div>
+              </a>
+            ))
+          }
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default gallery
