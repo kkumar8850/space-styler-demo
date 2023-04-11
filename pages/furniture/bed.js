4a6b127375f58d5ed4e3afebe83ac492dfeee054
Kuldeep <kuldeep.kumar@ikonwork.com>
2023-04-11 20:45:25 +0530

    add furniture section

--- pages/furniture/bed.js
+++ pages/furniture/bed.js
@@ -1,12 +1,90 @@
-import Header from '@/components/Header/Header'
 import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import Header from '@/components/Header/Header'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function bed() {
   return (
     <div>
         <Header />
-        <div className='container h-[90vh] flex items-center justify-center'>
-            <h1 className='text-6xl'>Bed Section Coming soon</h1>
+        <div>
+            <div 
+                className='h-[300px])'
+                style={{
+                    backgroundImage : `url(/Images/bed-images/image1.jpg)`,
+                    backgroundRepeat : 'no-repeat',
+                    backgroundSize : 'cover',
+                    backgroundPosition : 'center'
+                }}
+                >
+                <div 
+                    className='container h-[300px] relative bg-[rgba(0,0,0,0.6)]'>
+                    
+                        <div
+                        className='absolute bottom-[-30px]'>
+                        <div className='h-[200px] w-[100%] flex'>
+                            <AnimatePresence>
+                            <motion.div
+                                initial={{opacity : 0, y : 15}}
+                                animate={{opacity : 1, y : 0}}
+                                exit={{opacity : 0, y : 15}}
+                                transition={{ delay : 0.25}}
+                                className='h-[200px] flex'
+                            >
+                                <Image
+                                    src={`/Images/bed-images/image1.jpg`}
+                                    alt="test"
+                                    height={200}
+                                    width={200}
+                                    className="rounded-full"
+                                    />
+                            </motion.div>
+                                </AnimatePresence>
+                            <div>
+                                <h1 className='text-3xl ml-[30px] font-medium'>Bed</h1>
+                                {/* <p className='text-xl ml-[30px] mt-4'> 
+    Every interiors project and every client is unique so our approach can be flexible to suit your needs.</p> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='container mt-[60px]'>
+                <motion.div
+                    initial={{opacity : 0, y : -150}}
+                    animate={{opacity : 1, y : 0}}
+                    exit={{opacity : 0, y : -150}}
+                    transition={{ delay : 0.25}}
+                    >      
+                    <Link href="/furniture" className='border-secondary border-2 rounded-full px-8 py-2'>Back</Link>
+                </motion.div>
+                <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 mt-10 mb-10'>
+                    <AnimatePresence>
+                        {
+                            [0,1,2,3,4,5,6,7].map((a, index)=> (
+                                <motion.div
+                                    initial={{opacity : 0, y : 15}}
+                                    animate={{opacity : 1, y : 0}}
+                                    exit={{opacity : 0, y : 15}}
+                                    transition={{ delay : 0.25}}
+                                    className='h-[300px] relative w-[100%]'
+                                    key={a}
+                                >
+                                    <Image
+                                        src={`/Images/bed-images/image${index + 1}.jpg`}
+                                        alt={`images-${a}`}
+                                        fill
+                                        sizes="(max-width: 768px) 100vw,
+                                                (max-width: 1200px) 50vw,
+                                                33vw"
+                                    />
+                                </motion.div>
+                            ))
+                        }
+                    </AnimatePresence>
+                </div>
+            </div>
         </div>
     </div>
   )
--- pages/furniture/index.js
+++ pages/furniture/index.js
@@ -1,31 +1,98 @@
-import Header from '@/components/Header/Header'
 import React from 'react'
-import { MdOutlineUpcoming } from 'react-icons/md'
+import Header from '@/components/Header/Header'
+import Image1 from '@/public/Images/bed-images/image1.jpg'
+import Image2 from '@/public/Images/bed-images/image2.jpg'
+import Image from 'next/image'
+import { AnimatePresence, motion } from 'framer-motion'
 
 function index() {
   return (
         <div>
             <Header />
-            <div className='h-[500px]' style={{
-            backgroundImage : `url(/Images/1.jpg)`,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition:'center',
-            backgroundSize : 'cover'
-        }}>
-            <div>
-                <div className='h-[500px] container flex items-center justify-center'>
-                {/* <div className='text-center bg-[rgba(0,0,0,0.9)] h-[80%] w-[40%] rounded-full flex items-center justify-center'>
-                    <div>
-                    <h1 className='text-4xl text-secondary'>Our profile is unmatched.</h1>
-                    <p className=' w-[80%] mx-auto text-white'>Established in 2008, our interior design studio has redefined a new benchmark for remarkable interiors and exquisite furnishings. We truly believe that good quality design is life enhancing and deliver it in every project we undertake.</p>
-                    </div>
-                </div> */}
+            <div 
+                className='h-[500px] relative'
+                style={{
+                    backgroundImage : `url(/Images/furniture.jpg)`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition:'center',
+                    backgroundSize : 'cover'
+                }}
+            >
+            <div 
+                className='container h-[500px] bg-[rgba(0,0,0,0.5)] flex items-center justify-center'
+                > 
+                <div>
+                    <AnimatePresence>
+                        <motion.h1 
+                            className='text-5xl font-semibold'
+                            initial={{opacity : 0, x : -150}}
+                            animate={{opacity : 1, x : 0}}
+                            exit={{opacity : 0, x : -150}}
+                            transition={{ delay : 0.25}}
+                            >
+                                Discover the best furniture
+                        </motion.h1>
+                        <motion.p 
+                            className='text-2xl mt-4'
+                            initial={{opacity : 0, x : 150}}
+                            animate={{opacity : 1, x : 0}}
+                            exit={{opacity : 0, x : 150}}
+                            transition={{ delay : 0.25}}
+                            >
+                                The best choice for your comfort and everyone's style.
+                        </motion.p>
+                    </AnimatePresence>
                 </div>
             </div>
         </div>
-        <div className='container'>
-            <h1 className='text-5xl mt-[20px] text-center'>Coming Soonn</h1>
-        </div>
+            <AnimatePresence>
+                <div className='container my-8'>
+                    <div className='grid lg:grid-cols-6 sm:grid-cols-1 gap-4'>
+                        <motion.div 
+                            className='lg:col-start-2 lg:col-end-4'
+                            initial={{opacity : 0, x : -150}}
+                            animate={{opacity : 1, x : 0}}
+                            exit={{opacity : 0, x : -150}}
+                            transition={{ delay : 0.25}}
+                            >
+                            <a href={`/furniture/bed`} className="hover:mt-[-20px] transition-[0.5s]">
+                                <div 
+                                    className='bg-[#eaeaea] text-center p-[20px] rounded-sm ' >
+                                    <div>
+                                        <Image
+                                            src={Image1}
+                                            alt="second"
+                                            className='w-[100%] h-[300px] mb-4'
+                                        />
+                                    </div>
+                                    <p className='text-[#333] text-2xl font-bold mb-4'>Bed</p>
+                                </div>
+                            </a>
+                        </motion.div>
+                        <motion.div 
+                            className='lg:col-start-4 lg:col-end-6'
+                            initial={{opacity : 0, x : 150}}
+                            animate={{opacity : 1, x : 0}}
+                            exit={{opacity : 0, x : 150}}
+                            transition={{ delay : 0.25}}
+                            >
+                            <a href={`/furniture/bed`} className="hover:mt-[-20px] transition-[0.5s]">
+                                <div 
+                                    className='bg-[#eaeaea] text-center p-[20px] rounded-sm ' >
+                                    <div>
+                                        <Image
+                                            src={Image2}
+                                            alt="second"
+                                            className='w-[100%] h-[300px] mb-4'
+                                        />
+                                    </div>
+                                    <p className='text-[#333] text-2xl font-bold mb-4'>Sofa</p>
+                                </div>
+                            </a>
+                        </motion.div>
+                    </div>
+                </div>
+            </AnimatePresence>
     </div>
   )
 }
